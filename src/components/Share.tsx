ca4c80e616f4c8947accc7dc3c993a9459ebe1ac
therockstarind <rohitkumarrohit96@gmail.com>
2023-10-28 17:59:52 +0530

    Update Model

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,15 +1,13 @@
 import siteConfig from '../config/site.config';
 import { FiShare } from 'react-icons/fi';
-import { RiTwitterXFill } from 'react-icons/ri';
-import { BsCheck2, BsFacebook, BsLink45Deg, BsTelegram, BsWhatsapp } from 'react-icons/bs';
+import { RiShareFill } from 'react-icons/ri';
 import { GoReport } from 'react-icons/go';
 import { useClipboard } from 'use-clipboard-copy';
-import toast from 'react-hot-toast';
 import { getBaseUrl } from '../utils/getBaseUrl';
 import { useRouter } from 'next/router';
 import { Button, Image } from '@nextui-org/react';
-import Model from './UI/Model';
 import { useState } from 'react';
+import Modal from './UI/Model';
 
 export default function Share() {
   const { query, asPath } = useRouter();
@@ -17,32 +15,10 @@ export default function Share() {
   const hasFileExtension = /\.[^]+$/.test(asPath);
   const URL = `/api/og/?path=${hasFileExtension ? `${asPath}/` : `${asPath}`}`;
   const title = (query.path && Array.isArray(query.path) ? query.path[query.path.length - 1] : '').replaceAll('-', ' ').replaceAll('_', ' ');
-  const [isOpen, setIsOpen] = useState(false);
-
-
-  const shareOnWhatsApp = () => {
-    window.open(`https://api.whatsapp.com/send?text=${getBaseUrl()}${asPath}`);
-  };
-
-  const shareOnTelegram = () => {
-    window.open(`https://telegram.me/share/url?url=${getBaseUrl()}${asPath}`);
-  };
-
-  const shareOnTwitter = () => {
-    const text = `${title}`;
-    const url = `${getBaseUrl()}${asPath}`;
-    const hashtags = 'RDrive, RDRIVE.ORG';
-    const twitterIntentURL = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}&hashtags=${encodeURIComponent(hashtags)}`;
-    window.open(twitterIntentURL);
-  };
-
-  const shareOnFacebook = () => {
-    window.open(`https://www.facebook.com/sharer/sharer.php?u=${getBaseUrl()}${asPath}`);
-  };
+  const [showModal, setShowModal] = useState(false);
 
   const copyLinkToClipboard = () => {
     clipboard.copy(`${getBaseUrl()}${asPath}`);
-    toast('Link copied', { icon: <BsCheck2 /> });
   };
 
   const reportOnTelegram = () => {
@@ -66,63 +42,41 @@ export default function Share() {
   };
 
   return (
-    <Model isOpen={isOpen} onOpen={setIsOpen} 
-    content={<div className="flex flex-col items-center w-full mx-auto gap-3">
-    <Button isIconOnly className="bg-white bg-opacity-70 dark:bg-opacity-50 backdrop-blur-md border dark:border-gray-700 overflow-hidden dark:bg-black dark:text-white hover:bg-gray-100 dark:hover:bg-gray-850 rounded-full" onClick={shareCurrentUrl}><FiShare size={20}/> </Button>
-    <h1 className="text-xl font-semibold pr-3 line-clamp-1">Share {title}</h1>
-    <Image
-      src={URL}
-      alt={title}
-      width={1280}
-      height={640}
-      className='rounded-md'
-    />
-    <div className="flex flex-col w-full h-full gap-2">
-    <div className="flex items-center justify-between gap-2 xss:flex-wrap xsm:flex-nowrap">
-        <Button className="w-full bg-green-500 text-white" onPress={shareOnWhatsApp}>
-          <div className="flex items-center justify-center w-8 h-8">
-            <BsWhatsapp size={22} />
+    <main>
+      <Button
+        isIconOnly
+        className="bg-white bg-opacity-70 dark:bg-opacity-50 backdrop-blur-md border dark:border-gray-700 overflow-hidden dark:bg-black dark:text-white hover:bg-gray-100 dark:hover:bg-gray-850"
+        radius="full"
+        onPress={() => setShowModal(true)}
+      >
+        <FiShare size={18} />
+      </Button>
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <div className="flex flex-col items-center w-full mx-auto gap-3">
+          <h1 className="text-xl font-semibold pr-3 line-clamp-1">Share {title}</h1>
+          <Image src={URL} alt={title} width={1280} height={640} className="rounded-md" />
+          <div className="flex w-full h-full items-center justify-between gap-2">
+            <Button
+              className="w-full bg-indigo-500 text-white hover:bg-indigo-600"
+              onPress={shareCurrentUrl}
+            >
+              <div className="flex items-center justify-center w-8 h-8">
+                <RiShareFill size={22} />
+              </div>
+              <div>Share</div>
+            </Button>
+            <Button
+              className="w-full bg-red-600 text-white hover:bg-red-700"
+              onPress={reportOnTelegram}
+            >
+              <div className="flex items-center justify-center w-8 h-8">
+                <GoReport size={22} />
+              </div>
+              <div>Report Link</div>
+            </Button>
           </div>
-          <div>Share on WhatsApp</div>
-        </Button>
-        <Button className="w-full bg-blue-500 text-white" onPress={shareOnTelegram}>
-          <div className="flex items-center justify-center w-8 h-8">
-            <BsTelegram size={22} />
-          </div>
-          <div>Share on Telegram</div>
-        </Button>
-      </div>
-      <div className="flex items-center justify-between gap-2 xss:flex-wrap xsm:flex-nowrap">
-        <Button className="w-full bg-blue-400 text-white" onPress={shareOnTwitter}>
-          <div className="flex items-center justify-center w-8 h-8">
-            <RiTwitterXFill size={22} />
-          </div>
-          <div>Share on Twitter</div>
-        </Button>
-        <Button className="w-full bg-blue-800 text-white" onPress={shareOnFacebook}>
-          <div className="flex items-center justify-center w-8 h-8">
-            <BsFacebook size={22} />
-          </div>
-          <div>Share on Facebook</div>
-        </Button>
-      </div>
-      <div className="flex items-center justify-between gap-2">
-        <Button className="w-full " onPress={copyLinkToClipboard}>
-          <div className="flex items-center justify-center w-8 h-8">
-            <BsLink45Deg size={22} />
-          </div>
-          <div>Copy Link</div>
-        </Button>
-        <Button className="w-full bg-red-600 text-white" onPress={reportOnTelegram}>
-          <div className="flex items-center justify-center w-8 h-8">
-            <GoReport size={22} />
-          </div>
-          <div>Report Link</div>
-        </Button>
-      </div>
-    </div>
-  </div>}>
-    <Button isIconOnly className="bg-white bg-opacity-70 dark:bg-opacity-50 backdrop-blur-md border dark:border-gray-700 overflow-hidden  dark:bg-black dark:text-white hover:bg-gray-100 dark:hover:bg-gray-850" radius='full' onPress={() => setIsOpen(!isOpen)}><FiShare size={18}/> </Button>
-    </Model>
+        </div>
+      </Modal>
+    </main>
   );
 }
--- src/components/UI/Model.tsx
+++ src/components/UI/Model.tsx
@@ -1,41 +1,62 @@
-import React, { ReactNode, Dispatch, SetStateAction, useEffect } from 'react';
-import { Modal, ModalContent, ScrollShadow, useDisclosure } from '@nextui-org/react';
+"use client";
 
-interface ModelProps {
-  content: ReactNode | string;
-  isOpen: boolean;
-  onOpen: Dispatch<SetStateAction<boolean>>;
-  children?: ReactNode;
-}
+import { Dispatch, SetStateAction } from "react";
+import { Drawer } from "vaul";
+import * as Dialog from "@radix-ui/react-dialog";
+import useMediaQuery from "../../utils/hooks/use-media-query";
+import { cn } from "../../utils/utils";
 
-export default function Model({
-  content,
-  isOpen,
-  onOpen,
+export default function Modal({
   children,
-}: ModelProps): JSX.Element {
-  const { onOpen: onModalOpen } = useDisclosure();
+  className,
+  showModal,
+  setShowModal,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  showModal: boolean;
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+}) {
+  const { isMobile } = useMediaQuery();
 
-  useEffect(() => {
-    if (isOpen) {
-      onModalOpen();
-    }
-  }, [isOpen, onModalOpen]);
-
-  const handleModalClose = () => {
-    onOpen(false);
-  };
-
+  if (isMobile) {
+    return (
+      <Drawer.Root open={showModal} onOpenChange={setShowModal}>
+      <Drawer.Overlay className="fixed inset-0 z-40 bg-gray-100 bg-opacity-10 dark:bg-black dark:bg-opacity-10" />
+      <Drawer.Portal>
+        <Drawer.Content className="fixed bottom-0 left-0 right-0 z-50 rounded-t-xl border-t dark:border-none bg-white dark:bg-gray-850">
+        <div className="flex w-full items-center justify-center">
+            <div className="mt-3 h-1 w-12 rounded-full bg-gray-300" />
+        </div>
+        <div className="flex min-h-[150px] w-full items-center justify-center overflow-hidden my-3 px-1.5">
+        {children}
+        </div>
+        </Drawer.Content>
+        <Drawer.Overlay />
+      </Drawer.Portal>
+    </Drawer.Root>
+    );
+  }
   return (
-    <main>
-      <Modal isOpen={isOpen} onOpenChange={handleModalClose} hideCloseButton radius='none' scrollBehavior='inside' classNames={{ wrapper: "h-full w-full bottom-0", base: "fixed my-0 bottom-0 md:bottom-auto w-full max-w-full md:max-w-md sm:my-0 rounded-t-xl md:rounded-xl  overflow-y-scroll",}} >
-        <ModalContent className='p-2 md:p-4'>
-        <ScrollShadow>
-          {content}
-        </ScrollShadow>
-        </ModalContent>
-      </Modal>
-      {children}
-    </main>
+    <Dialog.Root open={showModal} onOpenChange={setShowModal}>
+      <Dialog.Portal>
+        <Dialog.Overlay
+          id="modal-backdrop"
+          className="animate-fade-in fixed inset-0 z-40 bg-gray-100 bg-opacity-10 dark:bg-black dark:bg-opacity-10"
+        />
+        <Dialog.Content
+          onOpenAutoFocus={(e) => e.preventDefault()}
+          onCloseAutoFocus={(e) => e.preventDefault()}
+          className={cn(
+            "animate-scale-in fixed inset-0 z-40 m-auto max-h-fit w-full max-w-md overflow-hidden border dark:border-none bg-white dark:bg-gray-850 p-0 shadow-xl md:rounded-2xl",
+            className,
+          )}
+        >
+          <div className="flex min-h-[150px] w-full items-center justify-center overflow-hidden my-2 px-1.5">
+           {children}
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
