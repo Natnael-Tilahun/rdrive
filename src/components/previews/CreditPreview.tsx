57203f5b3ffb24aa3e7a7a903230d68b4f5c0e18
therockstarind <rohitkumarrohit96@gmail.com>
2023-11-01 18:25:29 +0530

    Revert it for now will work on it later

--- src/components/previews/CreditPreview.tsx
+++ src/components/previews/CreditPreview.tsx
@@ -1,56 +1,46 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, ReactNode } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import rehypeRaw from 'rehype-raw';
+import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrowNight } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import 'katex/dist/katex.min.css';
 import useFileContent from '../../utils/fetchOnMount';
-import { MDXRemote } from 'next-mdx-remote';
-import { serialize } from 'next-mdx-remote/serialize';
-import MDXComponents from '../MDXComponents';
-import { Avatar } from '../GitHub/Avatar';
 import { Skeleton } from '@nextui-org/react';
-import { useRouter } from 'next/router';
+import { Avatar } from '../GitHub/Avatar';
 
-interface CreditPreviewProps {
+const CreditPreview: FC<{
   path: string;
-  standalone?: boolean;
-}
+}> = ({ path}) => {
+  const parentPath =  path;
 
-const CreditPreview: FC<CreditPreviewProps> = ({ path }) => {
-  const { asPath } = useRouter()
-  const { response: content, error, validating } = useFileContent(`/api/raw/?path=${asPath}/credit.md`, path);
-  const [mdxSource, setMdxSource] = useState<any>(null);
+  const { response: content, error, validating } = useFileContent(
+    `/api/raw/?path=${parentPath}/credit.md`,
+    path
+  );
 
-   {/* This is for testing purposes only on development builds */}
+  const customRenderer = {
+    code({ className, children, inline, ...props }: { className?: string | undefined; children: ReactNode; inline?: boolean }) {
+      if (inline) {
+        return <code className={className} {...props}>{children}</code>;
+      }
 
-  // const getMarkDownSource = async (content: string | null) => {
-  //   if (content) {
-  //     const mdxSource = await serialize(content, {
-  //       mdxOptions: {
-  //        development: process.env.NODE_ENV === "development"
-  //       },
-  //     });
-  //     setMdxSource(mdxSource);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getMarkDownSource(content);
-  // }, [content]);
-
-  const getMarkDownSource = async (content: string | null) => {
-    if (content) {
-      const mdxSource = await serialize(content);
-      setMdxSource(mdxSource);
-    }
+      const match = /language-(\w+)/.exec(className || '');
+      return (
+        <SyntaxHighlighter language={match ? match[1] : 'language-text'} style={tomorrowNight} PreTag="div" {...props}>
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      );
+    },
   };
-  
-  useEffect(() => {
-    getMarkDownSource(content);
-  }, [content]);
 
   if (error) {
     return (
       <Avatar username="therockstarind" />
     );
   }
-
   if (validating) {
     return (
       <div className="flex items-center gap-2" >
@@ -62,7 +52,8 @@ const CreditPreview: FC<CreditPreviewProps> = ({ path }) => {
         </div>
     );
   }
-  // Parse credit content and extract the name
+
+// Parse credit content and extract the name
 const creditMatches = content.match(/---\s*\n(.*?)\n---/s);
 let username = '';
 
@@ -79,12 +70,19 @@ if (creditMatches && creditMatches.length > 1) {
   }
 }
 
+
   return (
     <main>
-    <Avatar username={username} />
-      <MDXRemote {...mdxSource} components={MDXComponents}>
-      {content.replace(/---\s*\n.*?\n---/s, '')}
-      </MDXRemote>
+      <Avatar username={username} />
+      <ReactMarkdown
+        // @ts-ignore
+        remarkPlugins={[remarkGfm, remarkMath]}
+        // @ts-ignore
+        rehypePlugins={[rehypeKatex, rehypeRaw]}
+        components={customRenderer}
+      >
+        {content.replace(/---\s*\n.*?\n---/s, '')}
+      </ReactMarkdown>
     </main>
   );
 };
