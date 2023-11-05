5d4f29aea18f8d318007812d706385e643c4848e
therockstarind <rohitkumarrohit96@gmail.com>
2023-11-05 17:58:13 +0530

    do it false will write types and other stuff later

--- next.config.js
+++ next.config.js
@@ -42,7 +42,7 @@ module.exports = withBundleAnalyzer(
       ],
     },
     i18n,
-    reactStrictMode: true,
+    reactStrictMode: false,
     // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
     trailingSlash: true,
   })
