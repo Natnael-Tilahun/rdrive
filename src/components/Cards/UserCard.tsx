14e2977b47d7a2b9eabfc69ec037f5cdc0ec64e8
therockstarind <rohitkumarrohit96@gmail.com>
2023-11-06 18:00:56 +0530

    fix UserCard

--- src/components/Cards/UserCard.tsx
+++ src/components/Cards/UserCard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, Button, Card, CardBody, CardFooter, CardHeader } from "@nextui-org/react";
+import { Avatar, Card, CardBody, CardFooter, CardHeader } from "@nextui-org/react";
 import Link from "next/link";
 import { fetchGitHubUser } from '../../utils/githubApi';
 import Hover from "../UI/Tooltip";
@@ -24,53 +24,53 @@ export const UserCard = ({ username }) => {
   }, [username]);
 
   if (!userData) {
-    return null; 
+    return null;
   }
 
   return (
     <Card shadow="none" className="w-full md:w-80 border-none bg-transparent">
-      <CardHeader className="justify-between ">
+      <CardHeader className="justify-between">
         <div className="flex gap-3">
-          <Avatar className="bg-gray-200 dark:bg-gray-700" src={userData.avatar_url} alt={userData.login} size="lg"/>
+          <Avatar className="bg-gray-200 dark:bg-gray-700" src={userData?.avatar_url} alt={userData?.login} size="lg" />
           <div className="flex flex-col items-start justify-center">
-            <h4 className="text-lg font-semibold leading-none text-black dark:text-white">{userData.name || ''}</h4>
-            <Link href={`https://github.com/${userData.login}`} target="_blank" aria-label={userData.login}>
-              <h5 className="text-base tracking-tight text-default-500 dark:text-gray-400">@{userData.login}</h5>
+            <h4 className="text-lg font-semibold leading-none text-black dark:text-white">
+              {userData?.name || ''}
+            </h4>
+            <Link href={`https://github.com/${userData?.login}`} target="_blank" aria-label={userData?.login}>
+              <h5 className="text-base tracking-tight text-default-500 dark:text-gray-400">@{userData?.login}</h5>
             </Link>
           </div>
         </div>
-            <Link            
-              href={`https://github.com/${userData.login}`}
-              target="_blank">
-                      <BsGithub size={30} />
-            </Link>
+        <Link href={`https://github.com/${userData.login}`} target="_blank">
+          <BsGithub size={30} />
+        </Link>
       </CardHeader>
       <CardBody className="px-3 py-0">
         <p className="text-base text-default-500 dark:text-gray-200">
-          {userData.bio || 'Sorry, it appears that my GitHub bio is on a coffee break! ☕️'}
+          {userData?.bio || 'Sorry, it appears that my GitHub bio is on a coffee break! ☕️'}
         </p>
       </CardBody>
       <CardFooter className="gap-3 text-default-foreground justify-between">
         <div className="flex items-center space-x-2">
-        <FiUsers size={20}/>
-        <div className="flex gap-1">
-          <p className="font-semibold text-base">{userData.followers}</p>
-          <p className="text-base">Followers</p>
-        </div> 
+          <FiUsers size={20} />
+          <div className="flex gap-1">
+            <p className="font-semibold text-base">{userData?.followers}</p>
+            <p className="text-base">Followers</p>
+          </div>
         </div>
         <div className="flex space-x-3">
-          {userData.twitter_username && (
-            <Hover tipChildren="Flollow on X">
-            <Link href={`https://twitter.com/${userData.twitter_username}`} target="_blank">
-              <X />
-            </Link>
-          </Hover>
+          {userData?.twitter_username && (
+            <Hover tipChildren="Follow on X">
+              <Link href={`https://twitter.com/${userData.twitter_username}`} target="_blank">
+                <X />
+              </Link>
+            </Hover>
           )}
-          {userData.blog && (
+          {userData?.blog && (
             <Hover tipChildren="Visit Profile">
-            <Link href={userData.blog.startsWith("http") ? userData.blog : `https://${userData.blog}`} target="_blank">
-              <UserLink />
-            </Link>
+              <Link href={userData.blog.startsWith("http") ? userData.blog : `https://${userData.blog}`} target="_blank">
+                <UserLink />
+              </Link>
             </Hover>
           )}
         </div>
